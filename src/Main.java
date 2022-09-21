b8f81616db8c301e92c7a11c29c4927e8baa5a78
noah <noah.simma@htldornbirn.at>
2022-09-21 11:04:34 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(b
+    }
+}
