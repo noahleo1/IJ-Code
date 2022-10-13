f0d6066a8f55ee75e5dcd6ea0fe96219f4463a16
noah <noah.simma@htldornbirn.at>
2022-10-13 08:20:15 +0200

    first commit

---
+++ src/tictactoe.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class tictactoe {
+    public static void main(String[] args) {
+        boolean playerone = false;
+        System.out.println("Welcome to tictactoe");
+        System.out.println("Enter Your Choice Player 1 ");
+        Scanner scanner = new Scanner(System.in);
+        while (!playerone){
+
+
+        }
+
+
+    }
+}
