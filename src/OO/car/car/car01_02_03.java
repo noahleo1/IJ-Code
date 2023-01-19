a0da40128b8de44a690193cecb1b652aab81feea
noah <noah.simma@htldornbirn.at>
2023-01-19 09:03:44 +0100

    car ohne konstruktors nur 01-03

--- src/OO/car/car/car01_02_03.java
+++ src/OO/car/car/car01_02_03.java
@@ -1,28 +1,31 @@
 package OO.car.car;
 
-public class car {
-    public void breack(){
+public class car01_02_03 {
+    public void breack() {
         System.out.println("Ich bremse");
     }
-    public void drive(){
+
+    public void drive() {
         this.fuelAmount = this.fuelAmount - fuelConsuption;
         System.out.println("I am driving");
     }
-    public void superboost(){
-        if (this.totalfuelAmount*0.1<this.fuelAmount)  {
+
+    public void superboost() {
+        if (this.totalfuelAmount * 0.1 < this.fuelAmount) {
             System.out.println("SupperBoostMode is possible");
-        }
-        else {
+        } else {
             System.out.println("not enough fuel for Superboost");
         }
 
     }
-    public void honk(int amountOfReps){
-        
+
+    public void honk(int amountOfReps) {
+
         for (int i = 0; i < amountOfReps; i++) {
-          System.out.println("honk");
+            System.out.println("honk");
         }
     }
+
     public int fuelConsuption;
 
     public int totalfuelAmount;
@@ -32,9 +35,11 @@ public class car {
 
     private String color;
 
-
-
-
+    public car01_02_03(int fc, String b, String s){
+        this.fuelConsuption = fc;
+        this.brand = b;
+        this.serialNumber = s;
+    }
 
 
 }
