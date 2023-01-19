e7337b2ba95b4a57ff161413899340c1601b6abb
noah <noah.simma@htldornbirn.at>
2023-01-19 09:31:44 +0100

    car 05 begonnen

--- src/carPart2OO/car.java
+++ src/carPart2OO/car.java
@@ -1,4 +1,48 @@
 package carPart2OO;
 
+
 public class car {
+    public int fuelConsuption;
+
+    public int totalFuelAmount;
+    public int fuelAmount;
+    public String brand;
+    public String serialNumber;
+    private String color;
+
+    public car(int fuelConsuption, int totalFuelAmount, int fuelAmount, String brand, String serialNumber, String color) {
+        this.fuelConsuption = fuelConsuption;
+        this.totalFuelAmount = totalFuelAmount;
+        this.fuelAmount = fuelAmount;
+        this.brand = brand;
+        this.serialNumber = serialNumber;
+        this.color = color;
+    }
+
+    public void breack() {
+        System.out.println("Ich bremse");
+    }
+
+    public void drive() {
+        this.fuelAmount = this.fuelAmount - fuelConsuption;
+        System.out.println("I am driving");
+    }
+
+    public void superboost() {
+        if (this.totalfuelAmount * 0.1 < this.fuelAmount) {
+            System.out.println("SupperBoostMode is possible");
+        } else {
+            System.out.println("not enough fuel for Superboost");
+        }
+
+    }
+
+    public void honk(int amountOfReps) {
+
+        for (int i = 0; i < amountOfReps; i++) {
+            System.out.println("honk");
+        }
+    }
+
+
 }
